e15c92430d3ebd1948c81225525e4839d031a5a9
Abhishek Reddy <mail@abhishek.geek.nz>
2016-05-01 04:10:05 +1200

    Update README for 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jsr223-v8 "0.1.6"
+(defproject clj-jsr223-v8 "0.1.7-SNAPSHOT"
   :description "A JSR-223 (javax.script) API wrapping clj-v8"
   :url "https://github.com/arbscht"
   :license {:name "Eclipse Public License"
