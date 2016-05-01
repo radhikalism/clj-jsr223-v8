3a3a8ac24bd6d301a25a7606e6025d819a2099e1
Abhishek Reddy <mail@abhishek.geek.nz>
2016-05-02 00:23:33 +1200

    Bump to 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jsr223-v8 "0.1.7-SNAPSHOT"
+(defproject clj-jsr223-v8 "0.1.7"
   :description "A JSR-223 (javax.script) API wrapping clj-v8"
   :url "https://github.com/arbscht"
   :license {:name "Eclipse Public License"
